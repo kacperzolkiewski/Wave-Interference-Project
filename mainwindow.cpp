b8163bbf83d6b73bd171364d3edcf54dea7a4bfc
Szymon Pietraszek <szymekadr999@gmail.com>
2021-05-23 21:58:43 +0200

    empty click event added

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,3 +13,75 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_saveToFileBtn_clicked()
+{
+
+}
+
+
+void MainWindow::on_addSource1Btn_clicked()
+{
+
+}
+
+
+void MainWindow::on_addSource2Btn_clicked()
+{
+
+}
+
+
+void MainWindow::on_resetBtn_clicked()
+{
+
+}
+
+
+void MainWindow::on_sliderX_sliderMoved(int position)
+{
+
+}
+
+
+void MainWindow::on_sliderY_sliderMoved(int position)
+{
+
+}
+
+
+void MainWindow::on_sliderZ_sliderMoved(int position)
+{
+
+}
+
+
+void MainWindow::on_prevBtn_clicked()
+{
+
+}
+
+
+void MainWindow::on_nextBtn_clicked()
+{
+
+}
+
+
+void MainWindow::on_copyBtn_clicked()
+{
+
+}
+
+
+void MainWindow::on_startBtn_clicked()
+{
+
+}
+
+
+void MainWindow::on_stopBtn_clicked()
+{
+
+}
+
--- mainwindow.h
+++ mainwindow.h
@@ -15,6 +15,31 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_saveToFileBtn_clicked();
+
+    void on_addSource1Btn_clicked();
+
+    void on_addSource2Btn_clicked();
+
+    void on_resetBtn_clicked();
+
+    void on_sliderX_sliderMoved(int position);
+
+    void on_sliderY_sliderMoved(int position);
+
+    void on_sliderZ_sliderMoved(int position);
+
+    void on_prevBtn_clicked();
+
+    void on_nextBtn_clicked();
+
+    void on_copyBtn_clicked();
+
+    void on_startBtn_clicked();
+
+    void on_stopBtn_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
